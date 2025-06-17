59a6997207f8d8d8a90df5c6030331dd0629dcb4
Gurjant singh <raiboyofficel@gmail.com>
2025-06-17 14:21:59 +0530

    new.2

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+// <!-- Google tag (gtag.js) -->
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QB98GM5CQ3');
+
+
 // Navbar scroll effect
+
+
+
+
+
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) {
